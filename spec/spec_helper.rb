a9023badb6f77a5e8b846cfdbc5366c24199cf4a
sleeper <survivingknowledge@gmail.com>
2017-01-07 18:38:34 -0500

    stubbed rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,36 @@
+
+
+ENV["SINATRA_ENV"] = "test"
+
+require_relative '../config/environment'
+require 'rack/test'
+require 'capybara/rspec'
+require 'capybara/dsl'
+
+if ActiveRecord::Migrator.needs_migration?
+  raise 'Migrations are pending. Run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
+end
+
+ActiveRecord::Base.logger = nil
+
+RSpec.configure do |config|
+
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.include Rack::Test::Methods
+  config.include Capybara::DSL
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+def app
+  Rack::Builder.parse_file('config.ru').first
+end
+
+Capybara.app = app
