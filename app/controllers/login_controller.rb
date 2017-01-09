0305e036af50b88bc0f4169add971b12a0ce69b4
sleeper <survivingknowledge@gmail.com>
2017-01-09 02:22:47 -0500

    session error msg, logout

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -10,11 +10,26 @@ class LoginController < AppController
   end
 
   post '/login' do
-    user = User.find_by(:username => params[:user][:username])
-    if user && user.authenticate(params[:user][:password])
+    #reset login errortext
+    session["login_error"] = ""
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      #ok so if we don't redirect or render something it throws status must be >= 100
+      redirect '/'
+    else
+      session["login_error"] = "username and/or password failed"
+      redirect '/login'
     end
-    redirect '/'
+  end
+
+  get '/logout/?' do
+    if logged_in?
+      session.clear
+      redirect '/'
+    else
+      redirect '/login'
+    end
   end
 
 end
