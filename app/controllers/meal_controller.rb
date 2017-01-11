b0edff05b0809279ebc6ba19a7c5b7cdc6f4faff
sleeper <survivingknowledge@gmail.com>
2017-01-11 16:28:38 -0500

    meals controller, /meals, /meals/today, /meals/new stubbed

---
+++ app/controllers/meal_controller.rb
@@ -0,0 +1,46 @@
+class MealController < AppController
+
+
+  #get users meals for today only
+  get '/meals/today/?' do
+    if logged_in?
+      @meals = current_user.meals_today
+      erb :'/meals/meal_show'
+    else
+      session[:last_page] = '/meals/today'
+      redirect '/login'
+    end
+  end
+
+
+
+  #all meals user has
+  get '/meals/?' do
+    if logged_in?
+      @meals = current_user.meals
+      erb :'meals/meal_index'
+    else
+      session[:last_page] = '/meals'
+      redirect '/login'
+    end
+  end
+
+  get '/meals/new/?' do
+    if logged_in?
+
+      erb :'meals/meal_new'
+    else
+      session[:last_page] = '/meals/new'
+      redirect '/login'
+    end
+  end
+
+  #add meal
+  post '/meals' do
+    binding.pry
+  end
+
+
+
+
+end
--- config.ru
+++ config.ru
@@ -2,7 +2,7 @@ require './config/environment'
 
 use Rack::MethodOverride #needs to be first
 
-
+use MealController
 use FooditemController
 use LoginController
 run AppController
